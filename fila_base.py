d9f15d4ef3ba85c392c71957f41c1c045c5a0e50
Jonildo Parente <jonildoparente@gmail.com>
2020-04-28 15:04:38 -0400

    Atualizado padroes do mypy

--- fila_base.py
+++ fila_base.py
@@ -1,12 +1,13 @@
 import abc
+from typing import List
 
 from constantes import TAMANHO_PADRAO_MAX, TAMANHO_PADRAO_MIN
 
 
 class FilaBase(metaclass=abc.ABCMeta):
     codigo: int = 0
-    fila = []
-    clientes_atendidos = []
+    fila: List[str] = []
+    clientes_atendidos: List[str] = []
     senha_atual: str = ""
 
     def reseta_fila(self) -> None:
