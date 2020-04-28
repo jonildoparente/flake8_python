4dd63dbb43d2962a1705719c96b1e51ed1119bf6
Jonildo Parente <jonildoparente@gmail.com>
2020-04-28 12:37:35 -0400

    Refatoracao no projeto. Criado o metodo template em fila_base

--- fila_base.py
+++ fila_base.py
@@ -1,4 +1,7 @@
-class FilaBase:
+import abc
+
+
+class FilaBase(metaclass=abc.ABCMeta):
     codigo: int = 0
     fila = []
     clientes_atendidos = []
@@ -9,3 +12,20 @@ class FilaBase:
             self.codigo = 0
         else:
             self.codigo += 1
+
+    def insere_cliente(self):
+        self.fila.append(self.senha_atual)
+
+    """ Método template (Padrão de projetos) """
+    def atualiza_fila(self):
+        self.reseta_fila()
+        self.gera_senha_atual()
+        self.insere_cliente()
+
+    @abc.abstractmethod
+    def gera_senha_atual(self):
+        ...
+
+    @abc.abstractmethod
+    def chama_cliente(self, caixa: int):
+        ...
