996d5a7858942b390138eb93d295f65d8b4f9101
Jonildo Parente <jonildoparente@gmail.com>
2020-04-28 15:54:07 -0400

    Criado injecao de dependencias.

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fabrica_fila import FabricaFila
+from estatistica_detalhada import EstatisticaDetalhada
+from estatistica_resumida import EstatisticaResumida
 
 
-fila_nm = FabricaFila.pega_fila('normal')
+cx1 = 1
+cx2 = 2
+cx3 = 3
+
+fila_nm = FabricaFila.pega_fila('prioritaria')
 fila_nm.atualiza_fila()
 fila_nm.atualiza_fila()
 fila_nm.atualiza_fila()
@@ -10,13 +16,10 @@ fila_pr = FabricaFila.pega_fila('prioritaria')
 fila_pr.atualiza_fila()
 fila_pr.atualiza_fila()
 
-cx1 = 1
-cx2 = 2
-cx3 = 3
-
-
 print(fila_pr.chama_cliente(cx1))
 print(fila_pr.chama_cliente(cx3))
 print(fila_nm.chama_cliente(cx2))
 print(fila_pr.chama_cliente(cx2))
 print(fila_nm.chama_cliente(cx1))
+
+print(fila_nm.estatistica(EstatisticaResumida('10', 1098)))
